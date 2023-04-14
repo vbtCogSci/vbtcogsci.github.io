5dbbaf4211bcfe89e3c80620286b78575b1b60e6
vbtCogSci <64851765+vbtCogSci@users.noreply.github.com>
2023-04-14 19:42:16 +0100

    Update sliders.js

--- js/sliders.js
+++ js/sliders.js
@@ -370,7 +370,7 @@ function setupGameCanvas() {
                     addData(chart, new_step, [x, y, z, NaN, NaN, NaN]);
                 }
                 // Remove data if too much is plotted here max datapoints is 100
-                if (chart.data.datasets[0].data.length > 50) {
+                if (chart.data.datasets[0].data.length > 10/dt) {
                     removeData(chart);
                 }
             }
